e63125824407cbb5c98fe777e0039a51a20ff17a
xCHLOMIx <gutabarwaa@gmail.com>
2025-12-16 18:25:25 +0200

    Set up Neon Postgres w/ Drizzle

---
+++ src/models/user.model.js
@@ -0,0 +1,12 @@
+import { timestamp, serial, varchar } from 'drizzle-orm/pg-core';
+import { pgTable } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 255 }).notNull(),
+  email: varchar('email', { length: 255 }).notNull().unique(),
+  password: varchar('password', { length: 255 }).notNull(),
+  role: varchar('role', { length: 255 }).notNull().default('user'),
+  created_at: timestamp().defaultNow().notNull(),
+  updated_at: timestamp().defaultNow().notNull(),
+});
